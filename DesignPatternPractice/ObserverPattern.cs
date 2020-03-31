e98c3875d89ab77c25e6d8f8f8795a11864b990e
tmorrisiii86 <61118010+tmorrisiii86@users.noreply.github.com>
2020-03-31 11:32:31 -0500

    Add files via upload

---
+++ DesignPatternPractice/ObserverPattern.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPractice
+{
+    class ClassA
+    {
+        public int _myState;
+
+        public delegate void NotifyMyObservers(int change);
+        public NotifyMyObservers notify;
+
+        public ClassA()
+        {
+            _myState = 0;
+        }
+
+        public void ChangeMe(int newState)
+        {
+            _myState = newState;
+            notify(newState);
+        }
+    }
+
+    class FirstObserver
+    {
+        private ClassA whatIAmObserving;
+
+        public FirstObserver(ClassA theSource)
+        {
+            whatIAmObserving = theSource;
+            whatIAmObserving.notify += OnEvent;
+        }
+
+        public void OnEvent(int theChange)
+        {
+            Console.WriteLine("First Observer: I was notified ClassA changed. " + theChange);
+        }
+    }
+
+    class SecondObserver
+    {
+        private ClassA whatIAmObserving;
+
+        public SecondObserver(ClassA theSource)
+        {
+            whatIAmObserving = theSource;
+            whatIAmObserving.notify += OnEvent;
+        }
+
+        public void OnEvent(int theChange)
+        {
+            Console.WriteLine("Second Observer: I was notified ClassA changed. " + theChange);
+        }
+    }
+}
---
+++ DesignPatternPractice/SingletonDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPractice
+{
+    class SingletonDatabase
+    {
+        //have both public and private version of property
+        //property is static so it belongs to class itself
+
+        private static SingletonDatabase _instance = null;
+        public static SingletonDatabase instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new SingletonDatabase();
+                    //when class asked for public property, the constructor is called
+                }
+                return _instance;
+            }
+        }
+        private SingletonDatabase()
+        {
+            //constructor is private, so no other class can use it
+
+        }
+    }
+}
